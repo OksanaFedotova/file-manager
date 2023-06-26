9edc67cd84d6eb93b9900b8998070aafdbdd84db
OksanaFedotova <oxana-fedotova@yandex.ru>
2023-06-26 23:26:09 +0300

    feat: args parsing improved

--- src/utils/prepareArgs.js
+++ src/utils/prepareArgs.js
@@ -1,12 +1,22 @@
 export default (line) => {
   let res;
-  if (line.includes('\\')) {
-    res = line
-      .replaceAll(/\\ /gm, 'specialTemporaryPhraseForParsing')
+    if (line.includes('\\')) {
+    res = line.replaceAll(/\\ /gm, 'specialTemporaryPhraseForParsing');
+    if (res.includes("'")) {
+      res = res
+      .split(`'`)
+      .filter((el) => el !== ' ' && el !== '')
+      .map((el) => el.trim())
+      .map((el) => el.replaceAll('specialTemporaryPhraseForParsing', ' '))
+      .filter((el) => el !== ' ' && el !== '');
+    } else {
+      res = res
       .split(' ')
       .map((el) => el.replaceAll('specialTemporaryPhraseForParsing', ' '))
       .filter((el) => el !== ' ' && el !== '')
-  } else if(line.includes("'")) {
+    }
+  }
+   else if(line.includes("'")) {
     res = line 
       .split(`'`)
       .filter((el) => el !== ' ' && el !== '')
